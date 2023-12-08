294a28f7c5a96c948eb919768f26ef99db69f164
RrobertRr <rennert.robert@gmail.com>
2023-12-08 22:26:22 +0100

    add: Hawk.so

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, 'lib'))
 sys.path.append(os.path.join(parent_folder_path, 'plugin'))
 
-from plugin.main import PluginManager
+from hawkcatcher import Hawk
+hawk = Hawk("eyJpbnRlZ3JhdGlvbklkIjoiODc4OGU4MGEtYzEwMy00ZTMzLTg2MzYtNzkwMWRkNzQ1NGEwIiwic2VjcmV0IjoiZDNlZmI3NjMtMmEyYi00ODdlLTg4YzItZmViZTVhY2RmYzRlIn0=")
+
+try:
+    from plugin.main import PluginManager
 
 
-if __name__ == "__main__":
-    plugin_manager = PluginManager()
-    plugin_manager.run()
+    if __name__ == "__main__":
+        plugin_manager = PluginManager()
+        plugin_manager.run()
+except:
+    hawk.send()
