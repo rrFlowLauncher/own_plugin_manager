7c5b805f3b0440f51fb8a5917964d2458ce07e43
RrobertRr <rennert.robert@gmail.com>
2023-12-09 02:17:21 +0100

    add: Hawk.so bugfix: Hawk.so will now send every time a message to server over logging.error

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-import sys,os
+import sys, os
+
 parent_folder_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, 'lib'))
 sys.path.append(os.path.join(parent_folder_path, 'plugin'))
 
-from hawkcatcher import Hawk
-hawk = Hawk("eyJpbnRlZ3JhdGlvbklkIjoiODc4OGU4MGEtYzEwMy00ZTMzLTg2MzYtNzkwMWRkNzQ1NGEwIiwic2VjcmV0IjoiZDNlZmI3NjMtMmEyYi00ODdlLTg4YzItZmViZTVhY2RmYzRlIn0=")
-
-try:
-    from plugin.main import PluginManager
+from plugin.main import PluginManager
 
 
-    if __name__ == "__main__":
-        plugin_manager = PluginManager()
-        plugin_manager.run()
-except:
-    hawk.send()
+if __name__ == "__main__":
+    plugin_manager = PluginManager()
+#    plugin_manager.run(["install", "a", "b"])
+    plugin_manager.run()
